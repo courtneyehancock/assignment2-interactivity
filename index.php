4df97c99e58dcf4c5c8d0a417cd3c1b323a00320
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-13 16:30:52 -0700

    Delete folder

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>  
 <div class="container"> 
-  <div class="row index-row"> 
+  <div> 
     <?php 
-    if(have_posts()){             while(have_posts()){           the_post();?> 
-      <div class="one-half column posts"> 
+    if(have_posts()){             while(have_posts()){ 
+      the_post();?> 
+      <div> 
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> 
         <?php the_post_thumbnail('medium'); ?> 
         <p><?php echo "Published: " . get_the_date(); ?></p> 
