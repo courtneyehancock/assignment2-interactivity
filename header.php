b13591d74d3da6a30f8d92fa959b4733ce2aeb15
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-13 17:52:45 -0700

    Revert header back

--- header.php
+++ header.php
@@ -14,9 +14,18 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-3">
-          <h1> <a href="<?php get_home_url();?>"> <?php bloginfo('name');?> </a></h1> 
+          <?php if(get_header_image() == ' '){ ?>
+            <h1><a href="<?php get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1><?php
+          }else{?>
+            <a href="<?php get_home_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header() ->height; ?>" width="<?php echo get_custom_header()->width;?>" alt="Logo" /></a>
           <?php } ?>
+        </div>
+
+        <div class="col-lg-9 d-flex align-items-center">
+          <nav>
+            <p><a href="#">Navigation goes here</a></p>
+          </nav>
         </div>
       </div>
     </div>
-  </header>
+</header>
