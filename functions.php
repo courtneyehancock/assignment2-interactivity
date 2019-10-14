fd78d7ff42e52f1f8c7a422ecc2534126406d9eb
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-13 17:54:49 -0700

    Edit bootstrap styling

--- functions.php
+++ functions.php
@@ -10,14 +10,14 @@ Adds style sheet and JavaScript files
 
   function custom_theme_scripts() {
   //Bootstrap integration
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . 'css/bootstrap.min.css');
 
   //main CSS
     wp_enqueue_style('main-styles', get_stylesheet_uri());
 
   //Javascript files
-    wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/main.js');
-    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('custom-js', get_template_directory_uri() . 'js/main.js');
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . 'js/bootstrap.min.js');
 
   }
 
