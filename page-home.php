65e288b54a4c9add4188721ada8c47d3a4157234
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-26 12:14:10 -0700

    Edit footer widgets

--- page-home.php
+++ page-home.php
@@ -25,22 +25,6 @@ get_header(); ?>
         </div>
       </div>
 
-
-      <div class="row"
-        <div class="col-md-4">
-          <?php dynamic_sidebar('bottom-left-home-page'); ?>
-        </div>
-
-        <div class="col-md-4">
-          <?php dynamic_sidebar('bottom-middle-home-page'); ?>
-        </div>
-
-        <div class="col-md-4">
-          <?php dynamic_sidebar('bottom-right-home-page'); ?>
-        </div>
-
-      </div>
-
     </main>
   </div>
 
