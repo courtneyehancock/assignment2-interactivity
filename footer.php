5651eb1d1e92d501c95541b6b37c9f1757c9fac2
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-26 22:40:47 -0700

    Edit footer once again

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
-<footer>
-  <div class="container">
-    <div class="row footer-class">
-      <div class="col-md-4">
-        <?php dynamic_sidebar('bottom-left-home-page'); ?>
-      </div>
+  <footer>
+    <div class="container">
+      <div class="row footer-class">
+        <div class="col-md-4">
+          <?php dynamic_sidebar('bottom-left-home-page'); ?>
+        </div>
 
-      <div class="col-md-4">
-        <?php dynamic_sidebar('bottom-middle-home-page'); ?>
-      </div>
+        <div class="col-md-4">
+          <?php dynamic_sidebar('bottom-middle-home-page'); ?>
+        </div>
 
-      <div class="col-md-4">
-        <?php dynamic_sidebar('bottom-right-home-page'); ?>
+        <div class="col-md-4">
+          <?php dynamic_sidebar('bottom-right-home-page'); ?>
+        </div>
       </div>
     </div>
-  </div>
 
-</footer>
+  </footer>
   <?php wp_footer(); ?>
 </body>
 </html>
