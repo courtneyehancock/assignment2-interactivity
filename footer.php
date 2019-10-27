90e39bb93b1552960c441369d1b81834c171ea64
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-26 22:14:24 -0700

    Add container back to footer

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
 <footer>
-  <div class="row"
-    <div class="col-md-4">
-      <?php dynamic_sidebar('bottom-left-home-page'); ?>
-    </div>
+  <div class="container">
+    <div class="row footer-class"
+      <div class="col-md-4">
+        <?php dynamic_sidebar('bottom-left-home-page'); ?>
+      </div>
 
-    <div class="col-md-4">
-      <?php dynamic_sidebar('bottom-middle-home-page'); ?>
-    </div>
+      <div class="col-md-4">
+        <?php dynamic_sidebar('bottom-middle-home-page'); ?>
+      </div>
 
-    <div class="col-md-4">
-      <?php dynamic_sidebar('bottom-right-home-page'); ?>
+      <div class="col-md-4">
+        <?php dynamic_sidebar('bottom-right-home-page'); ?>
+      </div>
     </div>
-
   </div>
 
 </footer>
