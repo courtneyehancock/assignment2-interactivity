5cea05859a4d419fb27354c11ed42fcfd9baabe1
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-26 23:00:50 -0700

    Clean up custom page code

--- page-home.php
+++ page-home.php
@@ -7,20 +7,17 @@ Template Post Type: page
 
 get_header(); ?>
 
-  <main>
-    <!--Hero Image Section-->
-
-    <?php dynamic_sidebar('hero-image'); ?>
-
-    <div class="container">
+  <!--Hero Image Section-->
+  <?php dynamic_sidebar('hero-image'); ?>
+  <div class="container">
+    <main>
       <!--About Us Section-->
-
       <div class="row">
         <div class="col-md-12">
           <?php dynamic_sidebar('about-us'); ?>
         </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </div>
 
 <?php get_footer(); ?>
