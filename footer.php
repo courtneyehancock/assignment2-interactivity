1f66e577f3a92cdff34abd90724e7a3c54d9213e
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-26 22:36:55 -0700

    Edit footer class

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer>
   <div class="container">
     <div class="row footer-class">
-      <div class="col-md-3">
+      <div class="col-md-4">
         <?php dynamic_sidebar('bottom-left-home-page'); ?>
       </div>
 
-      <div class="col-md-3">
+      <div class="col-md-4">
         <?php dynamic_sidebar('bottom-middle-home-page'); ?>
       </div>
 
-      <div class="col-md-3">
+      <div class="col-md-4">
         <?php dynamic_sidebar('bottom-right-home-page'); ?>
       </div>
     </div>
