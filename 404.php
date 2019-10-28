722adc3c6013ddfd0c861c70f993e7094f59c0b8
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 19:04:37 -0700

    Code 404 php

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <main class="col-md-12">
+      <h2>Page not found</h2>
+      <p>I'm sorry, this never really happens. It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>home</a>?</p>
+      <img src="<?php get_template_directory_uri() . '/images/404.gif' ?>" alt="Error message" />
+    </main>
+  </div>
+</div>
+
+<?php get_footer(); ?>
