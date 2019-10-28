bc1aa12f6f26d493614d4f904989bd6eee5b4acf
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 19:36:03 -0700

    Edit footer styling again

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
   <footer>
     <div class="container">
-      <div class="row footer-class d-flex align-items-center">
-        <div class="col-md-4">
+      <div class="row">
+        <div class="col-sm-4">
           <?php dynamic_sidebar('bottom-left-home-page'); ?>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-sm-4">
           <?php dynamic_sidebar('bottom-middle-home-page'); ?>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-sm-4">
           <?php dynamic_sidebar('bottom-right-home-page'); ?>
         </div>
       </div>
