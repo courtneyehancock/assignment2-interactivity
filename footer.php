15f42275fcd70d05fd166b7be0fda57bc0a9769f
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 19:55:39 -0700

    Edit grid layout

--- footer.php
+++ footer.php
@@ -1,17 +1,15 @@
   <footer>
-      <div class="row">
-        <div class="col-sm-4">
-          <?php dynamic_sidebar('bottom-left-home-page'); ?>
-        </div>
-
-        <div class="col-sm-4">
-          <?php dynamic_sidebar('bottom-middle-home-page'); ?>
-        </div>
-
-        <div class="col-sm-4">
-          <?php dynamic_sidebar('bottom-right-home-page'); ?>
-        </div>
+    <div class="row">
+      <div class="col-sm">
+        <?php dynamic_sidebar('bottom-left-home-page'); ?>
       </div>
+      <div class="col-sm">
+        <?php dynamic_sidebar('bottom-middle-home-page'); ?>
+      </div>
+      <div class="col-sm">
+        <?php dynamic_sidebar('bottom-right-home-page'); ?>
+      </div>
+    </div>
 
   </footer>
   <?php wp_footer(); ?>
