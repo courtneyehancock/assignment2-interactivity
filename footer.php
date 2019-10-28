cd1785ebbcd006322f44b8e16c9e59b549416901
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 20:17:46 -0700

    Edit padding

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-4 p-2">
           <?php wp_nav_menu(array(
             'theme_location' => 'footer-menu',
             'container-class' => 'menu-footer'
             ));
           ?>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-4 p-2">
           <?php dynamic_sidebar('middle-footer'); ?>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-4 p-2">
           <?php dynamic_sidebar('right-footer'); ?>
         </div>
       </div>
