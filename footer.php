1d9b257c93759bba4958f3912175b476af6d8f93
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 19:56:34 -0700

    Edit container in footer

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
   <footer>
-    <div class="row">
-      <div class="col-sm">
-        <?php dynamic_sidebar('bottom-left-home-page'); ?>
-      </div>
-      <div class="col-sm">
-        <?php dynamic_sidebar('bottom-middle-home-page'); ?>
-      </div>
-      <div class="col-sm">
-        <?php dynamic_sidebar('bottom-right-home-page'); ?>
+    <div class="container-fluid d-flex">
+      <div class="row">
+        <div class="col-sm-4">
+          <?php dynamic_sidebar('bottom-left-home-page'); ?>
+        </div>
+        <div class="col-sm-4">
+          <?php dynamic_sidebar('bottom-middle-home-page'); ?>
+        </div>
+        <div class="col-sm-4">
+          <?php dynamic_sidebar('bottom-right-home-page'); ?>
+        </div>
       </div>
     </div>
 
