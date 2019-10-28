6be4e9413b92b745356f4c0bda08019507ab0390
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 20:21:38 -0700

    Edit sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,8 @@
 <?php dynamic_sidebar('sidebar'); ?>
+
+<?php if(is_page_template('page-right-sidebar.php')){
+  dynamic_sidebar('right-sidebar');
+}elseif(is_page_template('page-left-sidebar.php')){
+  Dynamic_sidebar('left-sidebar');
+}
+?>
