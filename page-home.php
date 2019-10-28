fcaadb77c8b956264229f515783bb3ca076a2338
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 19:43:08 -0700

    Remove main from about section

--- page-home.php
+++ page-home.php
@@ -9,15 +9,14 @@ get_header(); ?>
 
   <!--Hero Image Section-->
   <?php dynamic_sidebar('hero-image'); ?>
+
   <div class="container">
-    <main>
       <!--About Us Section-->
       <div class="row">
         <div class="col-md-12">
           <?php dynamic_sidebar('about-us'); ?>
         </div>
       </div>
-    </main>
   </div>
 
 <?php get_footer(); ?>
