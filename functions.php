4cb067a4b01d8e9ec183db616c14cafec5f00e68
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 20:11:49 -0700

    Edit sidebar php

--- functions.php
+++ functions.php
@@ -114,7 +114,50 @@ Adds style sheet and JavaScript files
       'after_title'     =>    '</h3'
     ));
 
-  }
+    //Sidebar Widget
+    register_sidebar(array(
+      'name'          => ('Sidebar'),
+      'id'            => 'sidebar',
+      'description'   => 'Sidebar widget area',
+      'before_widget' => '<div class="widget-sidebar">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>'
+    ));
 
+    //Left footer widget
+    register_sidebar(array(
+      'name'          => ('Left Footer'),
+      'id'            => 'left-footer',
+      'description'   => 'Left widget area in the footer',
+      'before_widget' => '<div class="widget-footer widget-left">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>'
+    ));
+
+    //Middle footer widget
+    register_sidebar(array(
+      'name'          => ('Middle Footer'),
+      'id'            => 'middle-footer',
+      'description'   => 'Middle widget area in the footer',
+      'before_widget' => '<div class="widget-footer widget-middle">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>'
+    ));
+
+    //Right footer widget
+    register_sidebar(array(
+      'name'          => ('Right Footer'),
+      'id'            => 'right-footer',
+      'description'   => 'Right widget area in the footer',
+      'before_widget' => '<div class="widget-footer widget-right">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>'
+    ));
+  }
   add_action('widgets_init', 'blank_widgets_init');
+
 ?>
