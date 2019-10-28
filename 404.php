4b767f4f350fb7639b84b464cf6f7f45d90a36a8
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 20:46:36 -0700

    Edit sidebar and 404.php

--- 404.php
+++ 404.php
@@ -1,13 +1,14 @@
+<?php /*Template Name: 404 Page*/?>
 <?php get_header(); ?>
 
 <div class="container">
-  <div class="row">
-    <main class="col-md-12">
-      <h2>Page not found</h2>
-      <p>I'm sorry, this never really happens. It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>home</a>?</p>
-      <img src="<?php get_template_directory_uri() . '/images/404.gif' ?>" alt="Error message" />
-    </main>
+<div class="row">
+  <div class="twelve columns not-found">
+    <h2>Page Not Found</h2>
+    <h3>This usually never happens.</h3>
+    <p>It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
   </div>
+</div>
 </div>
 
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,1 @@
 <?php dynamic_sidebar('right-sidebar'); ?>
-
-<?php if(is_page_template('page-right-sidebar.php')){
-  dynamic_sidebar('right-sidebar');
-}elseif(is_page_template('page-left-sidebar.php')){
-  Dynamic_sidebar('left-sidebar');
-}
-?>
