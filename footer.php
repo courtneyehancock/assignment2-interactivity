af0b2c0f27b0ce3b96cf2a47c530c3b10d5809ed
Courtney Hancock <ceh.courtney@gmail.com>
2019-10-27 20:16:01 -0700

    Edit footer layout

--- footer.php
+++ footer.php
@@ -1,23 +1,24 @@
-<div class="container">
-  <div class="row">
-    <div class="one-third column">
-      <?php wp_nav_menu(array(
-        'theme_location' => 'footer-menu',
-        'container-class' => 'menu-footer'
-        ));
-      ?>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <?php wp_nav_menu(array(
+            'theme_location' => 'footer-menu',
+            'container-class' => 'menu-footer'
+            ));
+          ?>
+        </div>
+        <div class="col-sm-4">
+          <?php dynamic_sidebar('middle-footer'); ?>
+        </div>
+        <div class="col-sm-4">
+          <?php dynamic_sidebar('right-footer'); ?>
+        </div>
+      </div>
     </div>
-    <div class="one-third column">
-      <?php dynamic_sidebar('middle-footer'); ?>
-    </div>
-    <div class="one-third column">
-      <?php dynamic_sidebar('right-footer'); ?>
-    </div>
-  </div>
-</div>
+  </footer>
 
-
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 
 </body>
 </html>
